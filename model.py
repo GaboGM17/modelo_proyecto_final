3d65a907e775465d7a674196695122d44be63979
Gabriel Guzman <gguzman@boss.tech>
2024-04-21 01:56:50 -0600

    Issue #1 adding the necesary libraries

---
+++ model.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+import joblib
